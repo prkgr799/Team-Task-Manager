e63360e556f679d729e6eb1375b6880cafb08118
prkgr799 <prkgr799@gmail.com>
2026-05-08 03:31:39 +0530

    Fix build: Switch to npm workspaces and update routing

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.get('/api/users', async (req, res) => {
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
   
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client', 'dist', 'index.html'));
   });
 }
